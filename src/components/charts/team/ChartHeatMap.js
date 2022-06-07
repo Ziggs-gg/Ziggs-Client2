75674b076321953e30a0226f27bf90ffe376bf57
Hyungjun <gudwnd1243@gmail.com>
2022-06-07 22:18:30 +0900

    Add: HeatMap, ChartLine 구현중

--- src/components/charts/team/ChartHeatMap.js
+++ src/components/charts/team/ChartHeatMap.js
@@ -3,14 +3,180 @@ import styled from 'styled-components';
 import theme from '../../../styles/theme.js';
 
 const ChartHeatmap = ({}) => {
-  return <ChartLayout></ChartLayout>;
+  return (
+    <ChartLayout>
+      <Label>포지션별 VERSUS 인덱스 비교</Label>
+      <TableContain>
+        <table>
+          <thead>
+            <HeatTr>
+              <LabelTd>team탑</LabelTd>
+              <LabelTd>team정글</LabelTd>
+              <LabelTd>team미드</LabelTd>
+              <LabelTd>team원딜</LabelTd>
+              <LabelTd>team서폿</LabelTd>
+            </HeatTr>
+          </thead>
+          <tbody>
+            <HeatTr>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+            </HeatTr>
+            <HeatTr>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+            </HeatTr>
+            <HeatTr>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+            </HeatTr>
+            <HeatTr>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+              <FirstTeamTd>00.00</FirstTeamTd>
+            </HeatTr>
+          </tbody>
+        </table>
+        <table>
+          <thead>
+            <HeatTr>
+              <SpaceTd />
+              <LabelTd>team탑</LabelTd>
+              <LabelTd>team정글</LabelTd>
+              <LabelTd>team미드</LabelTd>
+              <LabelTd>team원딜</LabelTd>
+              <LabelTd>team서폿</LabelTd>
+            </HeatTr>
+          </thead>
+          <tbody>
+            <HeatTr>
+              <ColumnLabel>전투력</ColumnLabel>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+            </HeatTr>
+            <HeatTr>
+              <ColumnLabel>생존력</ColumnLabel>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+            </HeatTr>
+            <HeatTr>
+              <ColumnLabel>골드수급력</ColumnLabel>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+            </HeatTr>
+            <HeatTr>
+              <ColumnLabel>시야장악력</ColumnLabel>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+              <SecondTeamTd>00.00</SecondTeamTd>
+            </HeatTr>
+          </tbody>
+        </table>
+      </TableContain>
+    </ChartLayout>
+  );
 };
 
 const ChartLayout = styled.div`
   width: 664px;
   height: 304px;
   margin-right: 32px;
-  background: red;
+`;
+
+const TableContain = styled.div`
+  display: flex;
+`;
+
+const Label = styled.div`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  align-items: center;
+  letter-spacing: -0.02em;
+  color: ${theme.white.white100};
+  margin-bottom: 16px;
+`;
+
+const HeatTr = styled.tr`
+  display: flex;
+`;
+
+const LabelTd = styled.td`
+  color: ${theme.white.white100};
+  width: 56px;
+  height: 24px;
+  text-align: center;
+  margin-right: 2px;
+  font-weight: 500;
+  font-size: 12px;
+  letter-spacing: -0.03em;
+`;
+
+const FirstTeamTd = styled.td`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${theme.white.white100};
+  width: 56px;
+  height: 56px;
+  border-radius: 10px;
+  background: ${theme.green.greenMain};
+  text-align: center;
+  margin: 0 2px 2px 0;
+`;
+
+const SecondTeamTd = styled.td`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${theme.white.white100};
+  width: 56px;
+  height: 56px;
+  border-radius: 10px;
+  background: ${theme.orange.orangeMain};
+  text-align: center;
+  margin: 0 2px 2px 0;
+`;
+
+const SpaceTd = styled.td`
+  width: 73px;
+  height: 24px;
+`;
+
+const ColumnLabel = styled.td`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 73px;
+  height: 56px;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.03em;
+  color: ${theme.white.white100};
+  text-align: center;
 `;
 
 export default ChartHeatmap;
--- src/components/charts/team/ChartLine.js
+++ src/components/charts/team/ChartLine.js
@@ -1,15 +1,119 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Line } from 'react-chartjs-2';
 import theme from '../../../styles/theme.js';
 
 const ChartLine = ({}) => {
-  return <ChartLayout></ChartLayout>;
+  // const { width, height } = props;
+
+  // const data = canvas => {
+  //   const ctx = canvas.getContext('2d');
+  //   const gradient = ctx.createLinearGradient(0, 0, 0, height);
+  //   gradient.addColorStop(0, 'rgba(250,174,50,1)');
+  //   gradient.addColorStop(1, 'rgba(250,174,50,0)');
+
+  //   return {
+  //     labels: [
+  //       '02:00',
+  //       '04:00',
+  //       '06:00',
+  //       '08:00',
+  //       '10:00',
+  //       '12:00',
+  //       '14:00',
+  //       '16:00',
+  //       '18:00',
+  //       '20:00',
+  //       '22:00',
+  //       '00:00',
+  //     ],
+  //     datasets: [
+  //       {
+  //         backgroundColor: gradient, // Put the gradient here as a fill color
+  //         borderColor: '#ff6c23',
+  //         borderWidth: 2,
+  //         pointColor: '#fff',
+  //         pointStrokeColor: '#ff6c23',
+  //         pointHighlightFill: '#fff',
+  //         pointHighlightStroke: '#ff6c23',
+  //         data: [
+  //           25.0, 32.4, 22.2, 39.4, 34.2, 22.0, 23.2, -24.1, 20.0, -18.4, 19.1,
+  //           17.4,
+  //         ],
+  //       },
+  //     ],
+  //   };
+  // };
+
+  const data = {
+    labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+    datasets: [
+      {
+        label: 'player1',
+        data: [33, 53, 32, 24, 44, 65, 53, 64, 45, 51],
+        fill: true,
+        backgroundColor: theme.green.greenOPA80,
+        borderColor: theme.green.greenB70,
+        pointStyle: 'circle',
+        pointRadius: 3,
+        pointHoverRadius: 5,
+      },
+      {
+        label: 'player2',
+        data: [33, 53, 32, 24, 44, 65, 86, 57, 48, 39],
+        fill: true,
+        backgroundColor: theme.orange.orangeOPA80,
+        borderColor: theme.orange.orangeB70,
+        pointStyle: 'circle',
+        pointRadius: 3,
+        pointHoverRadius: 5,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        grid: {
+          color: '#363634',
+          borderColor: '#363634',
+          tickColor: '#363634',
+          max: 100,
+          min: -100,
+        },
+      },
+      y: {
+        grid: {
+          color: '#363634',
+          borderColor: '#363634',
+          tickColor: '#363634',
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: '시간대별 평균 골드 격차',
+        color: theme.white.white100,
+        align: 'start',
+        padding: '15',
+      },
+    },
+  };
+
+  return (
+    <ChartLayout>
+      <Line data={data} options={options} />
+    </ChartLayout>
+  );
 };
 
 const ChartLayout = styled.div`
   width: 894px;
   height: 304px;
-  background: red;
 `;
 
 export default ChartLine;
