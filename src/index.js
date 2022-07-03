99f6bee3d180850d72db13eefacd95f05d2d95bf
Moonhyeseong <moonhs97@gmail.com>
2022-07-03 15:38:47 +0900

    Add: react-helmet setup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <Router />
+      <HelmetProvider>
+        <Router />
+      </HelmetProvider>
     </ThemeProvider>
   </>,
   document.getElementById('root')
