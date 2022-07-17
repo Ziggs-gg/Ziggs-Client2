e6e19a7907222f070176d33b5d9bb1b9e89e4e72
Moonhyeseong <moonhs97@gmail.com>
2022-07-17 15:10:28 +0900

    Add: react-helmet 메타태그 적용

--- src/pages/ComparePlayer/ComparePlayer.js
+++ src/pages/ComparePlayer/ComparePlayer.js
@@ -4,7 +4,8 @@ import SelectedCards from './components/selectedPlayer/SelectedCards';
 import ChartContainer from './components/ChartContainer';
 import HeatMapContainer from './components/heatMap/HeatMapContainer';
 import ChampionPool from './components/championPool/ChampionPool';
-import { Helmet } from 'react-helmet-async';
+import SEOMetaTag from '../../components/SEOMetaTag';
+
 const ComparePlayer = () => {
   const [selectedPlayers, setSelectedPlayers] = useState([]);
 
@@ -15,9 +16,12 @@ const ComparePlayer = () => {
 
   return (
     <>
-      <Helmet>
-        <title>VERSUS Pro Stats | 선수 비교</title>
-      </Helmet>
+      <SEOMetaTag
+        mainTitle={HELMET_CONTENT.mainTitle}
+        pageTitle={HELMET_CONTENT.pageTitle}
+        description={HELMET_CONTENT.description}
+        keywords={HELMET_CONTENT.keywords}
+      />
       <List
         setSelectedPlayers={setSelectedPlayers}
         selectedPlayers={selectedPlayers}
@@ -35,3 +39,11 @@ const ComparePlayer = () => {
 };
 
 export default ComparePlayer;
+
+const HELMET_CONTENT = {
+  mainTitle: 'VERSUS Pro Stats',
+  pageTitle: '선수 비교',
+  description:
+    'VERSUS.PS는 리그오브레전드 데이터를 시각화한 차트를 제공하고 있습니다. 응원하는 선수 혹은 구단의 데이터를 한 눈에 살펴보세요.라이벌 선수 혹은 구단의 지표를 함께 비교해보실 수 있습니다.',
+  keywords: '롤 지표사이트, LCK, LPL, LEC, pro stats, league of legend',
+};
