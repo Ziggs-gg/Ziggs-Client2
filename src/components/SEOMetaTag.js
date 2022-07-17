438597a50d177d0faeb5f635c3600a9148b4c0db
Moonhyeseong <moonhs97@gmail.com>
2022-07-17 17:02:00 +0900

    modify: meta tag index.html에 추가

--- src/components/SEOMetaTag.js
+++ src/components/SEOMetaTag.js
@@ -7,18 +7,6 @@ const SEOMetaTag = ({ mainTitle, pageTitle, description, keywords }) => {
       <title>{`${mainTitle} | ${pageTitle}`}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="VERSUS.PS" />
-      <meta
-        property="og:description"
-        content="여기를 눌러 링크를 확인하세요."
-      />
-      <meta
-        property="og:image"
-        content="https://user-images.githubusercontent.com/73605822/167045509-e0e7571a-bb6a-4411-9ad3-811a940cff4f.png"
-      />
-      <meta property="og:url" content="versus.ps/" />
-      <meta property="og:site_name" content="VERSUS.PS" />
     </Helmet>
   );
 };
